d6de764ffec876a72d5c36ff553d858ed9945fa2
Shabrina Dzakiyah <shabrinadzakiyah@gmail.com>
2024-08-20 12:49:29 +0700

    User

---
+++ app/src/main/java/com/example/project11vsga/ItemListNameAdapter.java
@@ -0,0 +1,69 @@
+package com.example.project11vsga;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project11vsga.model.User;
+
+import java.util.List;
+
+public class ItemListNameAdapter extends RecyclerView.Adapter<ItemListNameAdapter.ItemViewHolder> {
+
+    private List<User> userList;
+    private OnItemLongClickListener onItemLongClickListener;
+
+    public interface OnItemLongClickListener {
+        void onItemLongClick(User user);
+    }
+
+    public ItemListNameAdapter(List<User> userList, OnItemLongClickListener listener) {
+        this.userList = userList;
+        this.onItemLongClickListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_item_list_name_adapter, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        User user = userList.get(position);
+        holder.tvName.setText(user.getName());
+        holder.tvDomisili.setText(user.getDomisili());
+
+        holder.itemView.setOnLongClickListener(v -> {
+            if (onItemLongClickListener != null) {
+                onItemLongClickListener.onItemLongClick(user);
+            }
+            return true;
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return userList.size();
+    }
+
+    public void updateUserList(List<User> userList) {
+        this.userList = userList;
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvName, tvDomisili;
+
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvDomisili = itemView.findViewById(R.id.tvDomisili);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/project11vsga/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.project11vsga;
+
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.project11vsga.model.User;
+import com.example.project11vsga.repository.UserRepository;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ItemListNameAdapter itemListNameAdapter;
+    private UserRepository userRepository;
+    private FloatingActionButton btnAddItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        initalizeViews();
+        initializeOnClickListeners();
+    }
+
+    private void initalizeViews() {
+        recyclerView = findViewById(R.id.recyclerViewItemName);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        btnAddItem = findViewById(R.id.btnAddItem);
+    }
+
+    private void initializeOnClickListeners() {
+        btnAddItem.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AddUserActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadUserData();
+    }
+
+    private void loadUserData() {
+        userRepository = new UserRepository(this);
+        userRepository.open();
+
+        List<User> userList = userRepository.getAllUsers();
+        if (itemListNameAdapter == null) {
+            itemListNameAdapter = new ItemListNameAdapter(userList, this::showOptionsDialog);
+            recyclerView.setAdapter(itemListNameAdapter);
+        } else {
+            itemListNameAdapter.updateUserList(userList);
+            itemListNameAdapter.notifyDataSetChanged();
+        }
+
+        userRepository.close();
+    }
+
+    private void showOptionsDialog(User user) {
+        new AlertDialog.Builder(this)
+                .setTitle("Mau ngapain, nih?")
+                .setItems(new String[]{"Update", "Delete"}, (dialog, which) -> {
+                    switch (which) {
+                        case 0:
+                            Intent intent = new Intent(MainActivity.this, EditUserActivity.class);
+                            intent.putExtra("USER_ID", user.getId());
+                            startActivity(intent);
+                            break;
+                        case 1:
+                            deleteUser(user);
+                            break;
+                    }
+                })
+                .show();
+    }
+
+    private void deleteUser(User user) {
+        userRepository.open();
+        userRepository.deleteUser(user.getId());
+        userRepository.close();
+        loadUserData();
+        Toast.makeText(this, "User deleted", Toast.LENGTH_SHORT).show();
+    }
+}
